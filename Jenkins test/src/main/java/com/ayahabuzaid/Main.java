06ff6529b131dc57b78a1c74a91389588d4d536c
ayahabuzaidd <ayah1990abuzad@gmail.com>
2025-01-27 21:08:20 +0300

    main.java

--- Jenkins test/src/main/java/com/ayahabuzaid/Main.java
+++ Jenkins test/src/main/java/com/ayahabuzaid/Main.java
@@ -1,10 +1,7 @@
-{
-  "exclude": [
-    "**/bin",
-    "**/bower_components",
-    "**/jspm_packages",
-    "**/node_modules",
-    "**/obj",
-    "**/platforms"
-  ]
+package com.ayahabuzaid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
 }
