b7d9686878e649f77eccf78d597fcf162f4376c0
ayahabuzaidd <ayah1990abuzad@gmail.com>
2025-01-28 14:58:12 +0300

    test send notification

--- Jenkins test/src/main/java/com/ayahabuzaid/Main.java
+++ Jenkins test/src/main/java/com/ayahabuzaid/Main.java
@@ -2,6 +2,6 @@ package com.ayahabuzaid;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins!");
+        System.out.println("Helloo, Jenkins!");
     }
 }
